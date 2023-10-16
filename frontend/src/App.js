fa95a9ac785297c34d0930b1c2d1f0747c954e82
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-16 16:45:50 -0400

    Added Header and Footer files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from "./Components/Header"
 
 const App = () => {
   return (
-    <h1>Welcome to the Sahara Store</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to the Sahara Store</h1>
+
+        </Container>
+
+      </main>
+    
+    </>
   )
 }
 
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -4,9 +4,18 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa'
 const Header = () => {
   return (
     <header>
-      <Navbar bg= "dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg= "dark" variant="dark" expand="md" collapseOnSelect>   {/* to make hamburger menu appear sonner, use md instead of lg on expand */}
         <Container>
-            <Navbar href="/">Sahara Store</Navbar>
+            <Navbar.Brand href="/">Sahara Store</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className='ms-auto'>
+                    <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+                    <Nav.Link href="/login"><FaUser />Sign in</Nav.Link>
+
+                </Nav>
+
+            </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
