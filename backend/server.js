6143e55e1de52b7c2429b7ef78f3eb8633d9041c
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-18 21:58:52 -0400

    started to create backend; server.js and json file with products for test

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import products from './data/products.js';
+const port = process.env.PORT || 5000;      //To access a .env variable, you prefix it with process.env
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Api is running...')
+});
+
+// Route for all products
+app.get('/api/products', (req,res) => {
+    res.json(products);
+})
+
+// Route for a single product
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
