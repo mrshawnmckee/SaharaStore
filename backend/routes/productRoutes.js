b7e67608f94699d2cdc47524993e806007f30ceb
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-20 17:04:14 -0400

    added controller, tested functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,9 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from "../models/productModel.js";
+import { getProducts, getProductById } from "../controllers/productController.js";
 
-
-// Route for all products, the /api/products is not needed; it is in the server.js file(get all)
-router.get('/', asyncHandler(async (req,res) => {
-    const products = await Product.find({});    //Passing in empty object, because that will get al lof them
-    res.json(products);
-}));
-
-// Route for a single product(Get one)
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-        return res.json(product);
-    } else {
-        // If product is not found:
-        res.status(404);
-        throw new Error("Resource not found")
-    }
-}));
-
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById)
 
 
 export default router;
