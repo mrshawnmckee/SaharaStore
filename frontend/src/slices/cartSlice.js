fd2b6d4a2cd075e1e7edfea2ea5a836aede339cb
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-26 13:55:46 -0400

    added cart slice and starting cart functionality

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,36 @@
+// Since the requests are not asychronous, we dont need apiSLice, we use createSlice
+import { createSlice } from '@reduxjs/toolkit'
+
+// Items will be stored in local storage, so that if we leave the site and come back, they will still be in the cart
+// The localStorage.getItem("cart") checks for items in the cart, if not, it is an object cartitems with an empty array
+const initialState = localStorage.getItem("cart") ? JSON.parse (localStorage.getItem("cart")) : {cartItems: []}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        // This will take two things: current state and action, data inside of a payload(action.payload)
+        addToCart: (state, action) => {
+            const item = action.payload;
+
+            // Check to see if the item already exists in the cart
+            const existItem = state.cartItems.find((x) => x._id === item._id);
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
+            } else {
+                state.cartItems = [...state.cartItems, item];       //Adding the new item to the array
+            }
+
+            //Calculate items price
+            //Calculate shipping price
+            //Calculate tax price
+            //Calculate total price
+
+        }
+    },       //Any functions that have to do with the cart, ex add to cart, remove, etc
+
+})
+
+// inport this into store
+export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,17 @@
 // THis is the entry point of redux
 // Files concerning redux: store.js, constants.js, Provider in Index.js, slices folder, apiSlice.js, productsApiSlice
+// cartSlice.js
 //Taking productsapi slice into homescreen
 
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
+import cartSliceReducer from './slices/cartSlice'
 
 const store = configureStore({
+    // Global reducer
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
