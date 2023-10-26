1db860a028757c6043e4bc02d3d6c367c79652c6
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-25 21:25:57 -0400

    added slice file

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,14 @@
 // THis is the entry point of redux
+// Files concerning redux: store.js, constants.js, Provider in Index.js, slices folder, apiSlice.js
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;
